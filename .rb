abf4d0d17dbc3afd98d7528f2184e8f3f25b2ae2
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-02-20 23:52:33 -0500

    Create .rb

---
+++ .rb
@@ -0,0 +1,15 @@
+puts 'Glad you took the time to come to see your dear old granny? How are you?'
+bye = 0
+while bye < 3
+  answer = gets.chomp
+  puts 'HUH?! SPEAK UP, SONNY!'
+  answer = gets.chomp
+  if answer == answer.upcase
+    puts 'NO, NOT SINCE ' + (rand(21) + 1930).to_s + ' !'
+  elsif answer == 'BYE'
+    bye += 1
+  elsif bye == 3
+      puts 'BYE'
+      break
+  end
+end
